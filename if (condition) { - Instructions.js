a92db182158334fe1508f71a182b86c30c431bbd
Rafael Massimo <rafael_massimo@hotmail.com>
2023-05-16 16:38:03 -0700

    Start to use Javascript

---
+++ if (condition) { - Instructions.js
@@ -0,0 +1,24 @@
+if (condition) {
+    // true
+}
+else {
+    //false
+}
+
+for (let index = 0; index <= 10; index =+1){
+    //code
+}
+
+for (let index = 0; index < 10; index =+2){
+    //code
+}
+
+var index = 0;
+while(index < 10){
+    //code here
+}
+
+var index = 0;
+do {
+    //code here
+} while (index < 10)
